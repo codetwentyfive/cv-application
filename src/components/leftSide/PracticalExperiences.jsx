dd8504a07e084a959635407a28697e2400bb3df1
code25 <chingisenkhbaatar@gmail.com>
2024-02-03 00:16:31 +0100

    added details input

--- src/components/leftSide/PracticalExperiences.jsx
+++ src/components/leftSide/PracticalExperiences.jsx
@@ -1,92 +1,104 @@
 import Input from "./Input.jsx";
 
-function Company (props) {
+function Company(props) {
   return (
-    <form className = "place" data-index = {props.index}>
-      <div id = "header">
-        <div id = "title">
-          <input 
-            type = "checkbox" 
-            onChange = {props.showCompany} 
-            checked = {props.show ? true : false} 
+    <form className="place" data-index={props.index}>
+      <div id="header">
+        <div id="title">
+          <input
+            type="checkbox"
+            onChange={props.showCompany}
+            checked={props.show ? true : false}
           />
           <p>{props.company}</p>
         </div>
-        <div id = "control">
-          <button type = "button" onClick = {props.removeCompany}>Remove</button>
+        <div id="control">
+          <button type="button" onClick={props.removeCompany}>
+            Remove
+          </button>
         </div>
-        
       </div>
-      
-      <div id = "place-info">
-      <Input 
-        title = "Company"
-        type = "text" 
-        name = "company"
-        value = {props.company}
-        onChange = {props.changePracticalExperiences}
-      />
+
+      <div id="place-info">
+        <Input
+          title="Company"
+          type="text"
+          name="company"
+          value={props.company}
+          onChange={props.changePracticalExperiences}
+        />
+
+        <Input
+          title="Role"
+          type="text"
+          name="role"
+          value={props.role}
+          onChange={props.changePracticalExperiences}
+        />
+
+        <Input
+          title="Position"
+          type="text"
+          name="position"
+          value={props.position}
+          onChange={props.changePracticalExperiences}
+        />
+
+        <Input
+          title="Details"
+          type="textarea"
+          name="details"
+          value={props.details}
+          onChange={props.changePracticalExperiences}
         
-      <Input 
-        title = "Role"
-        type = "text" 
-        name = "role"
-        value = {props.role}
-        onChange = {props.changePracticalExperiences}
-      />
-      
-      <Input 
-        title = "Position"
-        type = "text" 
-        name = "position"
-        value = {props.position}
-        onChange = {props.changePracticalExperiences}
-      />
-      
-      <Input 
-        title = "From"
-        type = "date" 
-        name = "from"
-        value = {props.from}
-        onChange = {props.changePracticalExperiences}
-      />
-      
-      <Input 
-        title = "To"
-        type = "date" 
-        name = "to"
-        value = {props.to}
-        onChange = {props.changePracticalExperiences}
-      />
-      
+        />
+
+        <Input
+          title="From"
+          type="date"
+          name="from"
+          value={props.from}
+          onChange={props.changePracticalExperiences}
+        />
+
+        <Input
+          title="To"
+          type="date"
+          name="to"
+          value={props.to}
+          onChange={props.changePracticalExperiences}
+        />
       </div>
     </form>
   );
 }
 
-function PracticalExperiences (props) {
+function PracticalExperiences(props) {
   return (
     <>
       {props.practicalExperiences.map((place, index) => {
-        return ( 
+        return (
           <Company
-            from = {place.from}
-            to = {place.to}
-            role = {place.role}
-            position = {place.position}
-            company = {place.company}
-            index = {index}
-            key = {place.id}
-            show = {place.show}
-            changePracticalExperiences = {props.changePracticalExperiences}
-            removeCompany = {props.removeCompany}
-            showCompany = {props.showCompany}
+            from={place.from}
+            to={place.to}
+            role={place.role}
+            position={place.position}
+            company={place.company}
+            details={place.details}
+            index={index}
+            key={place.id}
+            show={place.show}
+            changePracticalExperiences={props.changePracticalExperiences}
+            removeCompany={props.removeCompany}
+            showCompany={props.showCompany}
           />
-        )
+        );
       })}
-      <a href = "#" onClick = {props.newCompany}>+ Add company</a>
+      <a href="#" onClick={props.newCompany}>
+        + Add company
+      </a>
     </>
   );
 }
 
-export default PracticalExperiences
+export default PracticalExperiences;
